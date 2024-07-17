93ca525badb28c0d77294ba8721837e967c98b47
Rembox <Rembox@R422>
2024-07-17 20:14:27 +0300

    Добавьте файлы проекта.

---
+++ 8kyu_CountOfPositives/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8kyu_CountOfPositives
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] input = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 };
+            int[] result = Kata.CountPositivesSumNegatives(input);
+            Console.WriteLine($"[{result[0]}, {result[1]}]");
+        }
+    }
+}
---
+++ 8kyu_CountOfPositives/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+/*
+Given an array of integers.
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+If the input is an empty array or is null, return an empty array.
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]
+*/
+
+namespace _8kyu_CountOfPositives
+{
+    public class Kata
+    {
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+            if (input == null || input.Length == 0) return new int[0];
+            int sumCount = 0;
+            int sumNegative = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 0:
+                        continue;
+                    case int num when num > 0:
+                        sumCount += 1;
+                        break;
+                    case int num when num < 0:
+                        sumNegative += num;
+                        break;
+                }
+            }
+            return new int[] { sumCount, sumNegative };
+        }
+    }
+}
