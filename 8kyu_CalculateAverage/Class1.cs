10a276f28a500ec7b1150790afd1cde14eca8280
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-07-17 20:40:32 +0300

    Add files via upload

---
+++ 8kyu_CalculateAverage/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _8kyu_CalculateAverage
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] array = { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 };
+            double result = AverageSolution.FindAverage(array);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 8kyu_CalculateAverage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/*
+Write a function which calculates the average of the numbers in a given list.
+Note: Empty arrays should return 0. 
+ */
+namespace _8kyu_CalculateAverage
+{
+    public class AverageSolution
+    {
+        public static double FindAverage(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                double sum = 0;
+                foreach (var item in array)
+                {
+                    sum += item;
+                }
+                return sum / array.Length;
+            }
+        }
+    }
+}
