d90948fdd9090472921d258a30491c2de0c934ae
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-07-18 20:31:01 +0300

    Add files via upload

---
+++ 8kyu_ConvertNumberToString_/8kyu_ConvertNumberToString_/Class1.cs
@@ -0,0 +1,15 @@
+﻿using _8kyu_ConvertNumberToString;
+using System;
+
+namespace _8kyu_ConvertNumberToString
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int input = -522+1;
+            string result = Kata.NumberToString(input);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 8kyu_ConvertNumberToString_/8kyu_ConvertNumberToString_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+/*
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+*/
+
+namespace _8kyu_ConvertNumberToString
+{
+    public class Kata
+    {
+        public static string NumberToString(int num)
+        {
+            return num.ToString();
+        }
+    }
+}
