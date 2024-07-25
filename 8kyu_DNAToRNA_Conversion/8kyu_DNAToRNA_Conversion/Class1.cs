ad8b455eb177e7d349372d40dc61e10428a146c6
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-07-25 08:58:57 +0300

    Add files via upload

---
+++ 8kyu_DNAToRNA_Conversion/8kyu_DNAToRNA_Conversion/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Converter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string dna = "GUTGCCAT";
+            string rna = Converter.DnaToRna(dna);
+            Console.WriteLine(rna);
+
+            string dna2 = "GATTACA";
+            Console.WriteLine(Converter.DnaToRna(dna2));
+        }
+    }
+}
---
+++ 8kyu_DNAToRNA_Conversion/8kyu_DNAToRNA_Conversion/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Converter
+{
+    public class Converter
+    {
+        public string DnaToRna(string dna)
+        {
+            if (string.IsNullOrEmpty(dna))
+            {
+                return string.Empty;
+            }
+            return dna.Replace('T', 'U');
+        }
+    }
+}
