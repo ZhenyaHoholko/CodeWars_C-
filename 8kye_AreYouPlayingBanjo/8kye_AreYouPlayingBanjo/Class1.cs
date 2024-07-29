038a2a13ca92940c272b97f4da660bf8f83c1663
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-07-29 19:10:54 +0300

    Add files via upload

---
+++ 8kye_AreYouPlayingBanjo/8kye_AreYouPlayingBanjo/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8kye_AreYouPlayingBanjo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string name = "Robert";
+            double result = _8kye_AreYouPlayingBanjo.AreYouPlayingBanjo(name);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 8kye_AreYouPlayingBanjo/8kye_AreYouPlayingBanjo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Kata
+{
+    public static string AreYouPlayingBanjo(string name)
+    {
+        if (name == null)
+        {
+            return "Invalid input";
+        }
+        if (name.ToLower()[0] == 'r')
+        {
+            return name + " plays banjo";
+        }
+        else
+        {
+            return name + " does not play banjo";
+        }
+    }
+}
