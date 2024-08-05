1ea1fb0b78b14717f156224af43a581e1fea31e7
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-08-05 20:32:20 +0300

    Add files via upload

---
+++ 8kyu_ReversedStrings/8kyu_ReversedStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ Complete the solution so that it reverses the string passed into it.
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+public static class Kata
+{
+    public static string Solution(string str)
+    {
+        char[] oldStr = str.ToCharArray();
+        char[] newStr = new char[oldStr.Length];
+        for (int i = 0; i < oldStr.Length; i++)
+        {
+            newStr[oldStr.Length - 1 - i] = oldStr[i];
+        }
+        return new string(newStr);
+        throw new NotImplementedException("TODO: Kata.Solution(string) => string");
+    }
+}
