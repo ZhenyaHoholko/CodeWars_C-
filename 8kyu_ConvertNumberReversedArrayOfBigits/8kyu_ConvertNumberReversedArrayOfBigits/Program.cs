cb26d9a0c75c3bf1e8de96476678105486391f8d
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-08-20 20:45:56 +0300

    Add files via upload

---
+++ 8kyu_ConvertNumberReversedArrayOfBigits/8kyu_ConvertNumberReversedArrayOfBigits/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            return n.ToString()
+                    .Reverse()
+                    .Select(ch => (long)char.GetNumericValue(ch))
+                    .ToArray();
+        }
+    }
+}
