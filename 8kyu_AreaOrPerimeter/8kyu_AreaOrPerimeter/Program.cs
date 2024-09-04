34ac7b6349106cc919d392c2eacc05b668ecd8d6
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-04 20:05:05 +0300

    Add files via upload

---
+++ 8kyu_AreaOrPerimeter/8kyu_AreaOrPerimeter/Program.cs
@@ -0,0 +1,11 @@
+﻿public class MathCheck
+{
+    public static int AreaOrPerimeter(int l, int w)
+    {
+        if (l == w ) return l*w;
+        else
+        {
+            return (l + w) * 2;
+        }
+    }
+}
