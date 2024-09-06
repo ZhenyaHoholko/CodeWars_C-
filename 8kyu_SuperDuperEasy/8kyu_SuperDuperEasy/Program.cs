963a945ed113f6f8b9b1c443b2a5782449eb18b4
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-06 20:08:00 +0300

    Add files via upload

---
+++ 8kyu_SuperDuperEasy/8kyu_SuperDuperEasy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Kata
+{
+    public static string Problem(string a)
+    {
+        if (double.TryParse(a, out var result))
+        {
+            return ((result * 50) + 6).ToString();
+        }
+        else
+        {
+            return "Error";
+        }
+    }
+}
