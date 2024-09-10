5775b657f1a130ac6c2a0e8e76a4d24cf30e7dea
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-10 14:20:42 +0300

    Add files via upload

---
+++ 7kyu_AnagramDetection/7kyu_AnagramDetection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace MyNamespace
+{
+    public class Kata
+    {
+        public static bool IsAnagram(string a, string b)
+        {
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+            var sortedA = new string(a.ToLower().OrderBy(c => c).ToArray());
+            var sortedB = new string(b.ToLower().OrderBy(c => c).ToArray());
+            return sortedA == sortedB;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
