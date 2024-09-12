1b10a8fb220d1e7fb92a973b50e82503adbdd53e
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-12 15:59:32 +0300

    Add files via upload

---
+++ 7kyu_SortArrayStringlength/7kyu_SortArrayStringlength/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Kata
+{
+    public static string[] SortByLength(string[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j].Length> array[j + 1].Length)
+                {
+                    string temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
