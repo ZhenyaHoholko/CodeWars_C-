1371ea63ea5d1465d49bee8a7b8478364d818307
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-24 11:47:29 +0300

    Add files via upload

---
+++ 7kyu_YouAreSquare!/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Kata
+{
+    public static bool IsSquare(int n)
+    {
+        if (n < 0)
+        {
+            return false;
+        }
+        double sqrt = Math.Sqrt(n);
+        return sqrt == (int)sqrt;
+    }
+}
