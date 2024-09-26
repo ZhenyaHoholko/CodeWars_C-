5a913a1d44de68a6631e7e8b86b848604e773ba3
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-26 19:37:06 +0300

    Add files via upload

---
+++ 7kyu_MakeFunctionThatDoesArithmetic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Kata
+{
+    public static double Arithmetic(double a, double b, string op)
+    {
+        switch (op)
+        {
+            case "add":
+                return a + b;
+            case "subtract":
+                return a - b;
+            case "multiply":
+                return a * b;
+            case "divide":
+                return a / b;
+            default:
+                throw new ArgumentException("Unknown operation");
+        }
+    }
+}
