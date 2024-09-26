0f84d5c5cb800ff8c4e46e850eb82322d4a21431
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-26 20:00:32 +0300

    Add files via upload

---
+++ 7kyu_Ones&Zeros/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Solution
+{
+    class Kata
+    {
+        public  static int Calculate(int n,int numb)
+        {
+            return n * (int)Math.Pow(2, numb);
+        }
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            int lengthArr = BinaryArray.Length;
+            int finalNumber = 0;
+            for (int i = 0; i < lengthArr; i++)
+            {
+                finalNumber += Calculate(BinaryArray[lengthArr - i - 1], i);
+            }
+            return finalNumber;
+        }
+    }
+}
