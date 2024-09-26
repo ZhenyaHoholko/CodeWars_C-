e93a18a45a43ff815786667d3c60b0fec406cd25
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-26 19:20:39 +0300

    Add files via upload

---
+++ 8kyu_CountByX/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int[] CountBy(int x, int n)
+    {
+        int[] z = new int[n];
+        int counter = 0;
+        for (int i = 1;; i++)
+        {
+            if(i%x== 0)
+            {
+                z[counter] = i;
+                counter++;
+            }
+            if (counter>= n)
+            {
+                break;
+            }
+        }
+        return z;
+    }
+}
