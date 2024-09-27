5ef40bdc3098ebdbb94032a44ec9a3d5b7b656e3
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-09-27 19:34:49 +0300

    Add files via upload

---
+++ 7kyu_FixStringCase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string Solve(string s)
+    {
+        int upperCount = s.Count(char.IsUpper);
+        int lowerCount = s.Count(char.IsLower);
+        if (upperCount > lowerCount)
+        {
+            return s.ToUpper();
+        }
+        else
+        {
+            return s.ToLower();
+        }
+    }
+}
