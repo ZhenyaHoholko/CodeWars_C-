3817426dc2430de531715cf337c8bf28d5cb6a7d
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-01 18:43:18 +0300

    Add files via upload

---
+++ 6kyu_TakeTenMinutesWalk/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Kata
+{
+    public static bool IsValidWalk(string[] walk)
+    {
+        int nCount = 0, eCount = 0, wCount = 0, sCount = 0;
+        if (walk.Length != 10)
+        {
+            return false;
+        }
+        for (int i = 0; i < walk.Length; i++)
+        {
+            if (walk[i] == "n") { nCount++; }
+            if (walk[i] == "s") { sCount++; }
+            if (walk[i] == "e") { eCount++; }
+            if (walk[i] == "w") { wCount++; }
+        }
+        return nCount == sCount && eCount == wCount;
+    }
+}
