28e3c3733b3e201e75a64c46779e9b731ec39d88
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-04 20:06:34 +0300

    Add files via upload

---
+++ 6kyu_CountingDuplicates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static int DuplicateCount(string str)
+    {
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+        str = str.ToLower();
+        foreach (char c in str)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+        }
+        int duplicateCount = 0;
+        foreach (var count in charCount.Values)
+        {
+            if (count > 1)
+            {
+                duplicateCount++;
+            }
+        }
+
+        return duplicateCount;
+    }
+}
