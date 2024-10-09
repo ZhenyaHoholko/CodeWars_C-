050ac0262596087849878086ed09ec80c437d039
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-09 20:54:42 +0300

    Add files via upload

---
+++ 8kyu_CountingSheep/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int CountSheeps(bool[] sheeps)
+    {
+        if (sheeps == null) return 0;
+        return sheeps.Count(sheep => sheep == true);
+    }
+}
