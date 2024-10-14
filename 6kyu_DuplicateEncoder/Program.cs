e9e93fcc1f867fabf59bfa8db13a83f024aa59bd
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-14 20:11:49 +0300

    Add files via upload

---
+++ 6kyu_DuplicateEncoder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+public class Kata
+{
+    public static string DuplicateEncode(string word)
+    {
+        string result = "";
+        char[] characters = word.ToLower().ToCharArray();
+
+        foreach (char character in characters)
+        {
+            if (characters.Count(c => c == character) > 1)
+            {
+                result += ")";
+            }
+            else
+            {
+                result += "(";
+            }
+        }
+        return result;
+    }
+}
