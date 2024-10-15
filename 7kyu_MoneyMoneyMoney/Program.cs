808a1533f13f211e1218303c5541a2293dc3e3ce
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-15 17:02:56 +0300

    Add files via upload

---
+++ 7kyu_MoneyMoneyMoney/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Kata
+{
+    public static int CalculateYears(double principal, double interest, double tax, double desiredPrincipal)
+    {
+        double sumRes = principal;
+        int years = 0;
+        if (principal >= desiredPrincipal)
+        {
+            return 0;
+        }
+        while (sumRes < desiredPrincipal)
+        {
+            double yearlyInterest = sumRes * interest;
+            double netInterest = yearlyInterest * (1 - tax);
+            sumRes += netInterest;
+            years++;
+        }
+
+        return years;
+    }
+}
