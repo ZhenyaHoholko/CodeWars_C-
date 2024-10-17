59c2bbfc69aa0de01761a2d8078152ff76017aba
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-17 14:33:09 +0300

    Add files via upload

---
+++ 8kyu_KeepUpHoop/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class Kata
+{
+    public static string HoopCount(int n)
+    {
+        if (n >= 10)  return "Great, now move on to tricks";
+        else return "Keep at it until you get it";
+    }
+}
