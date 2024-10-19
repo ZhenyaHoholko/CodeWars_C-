73747ebac56f05a5dd514cf61b120bcfa4a39d61
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-19 15:09:41 +0300

    Add files via upload

---
+++ 7kyu_CheckExam/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int CheckExam(string[] arr1, string[] arr2)
+    {
+        int score = 0;
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr2[i] == "")
+            {
+                continue;
+            }
+            else if (arr1[i] == arr2[i])
+            {
+                score += 4;
+            }
+            else // Неправильный ответ
+            {
+                score -= 1;
+            }
+        }
+        return score < 0 ? 0 : score;
+    }
+}
