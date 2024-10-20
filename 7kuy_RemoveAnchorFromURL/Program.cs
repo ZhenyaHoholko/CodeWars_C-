3d3e5128101767765afa9eead92c99223a1569a0
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-20 22:42:34 +0300

    Add files via upload

---
+++ 7kuy_RemoveAnchorFromURL/Program.cs
@@ -0,0 +1,12 @@
+﻿//Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+//Examples
+//"www.codewars.com#about" --> "www.codewars.com"
+//"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+public static class Kata
+{
+    public static string RemoveUrlAnchor(string url)
+    {
+        int anchorIndex = url.IndexOf('#');
+        return anchorIndex == -1 ? url : url.Substring(0, anchorIndex);
+    }
+}
