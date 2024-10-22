75eae1e91d6f0d6bdb4d3bce22dd8fc3ad8cc2b1
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-22 20:13:19 +0300

    Add files via upload

---
+++ 7kyu_ShortestWord/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+    public static int FindShort(string s)
+    {
+        string[] words = s.Split(' ');
+        int shortestLength = int.MaxValue;
+        foreach (string word in words)
+        {
+            if (word.Length < shortestLength)
+            {
+                shortestLength = word.Length;
+            }
+        }
+        return shortestLength;
+    }
+}
