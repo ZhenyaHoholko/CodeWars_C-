f045ffec154303e5c21a819be8375dcf6d3e74ce
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-26 21:01:29 +0300

    Add files via upload

---
+++ 6kyu_SimpleFunStolenLunch/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public string StolenLunch(string note)
+        {
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+            char[] ar = note.ToCharArray();
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (char.IsDigit(ar[i]))
+                {
+                    ar[i] = letters[ar[i] - '0'];
+                }
+                else if (ar[i] >= 'a' && ar[i] <= 'j')
+                {
+                    ar[i] = (char)('0' + Array.IndexOf(letters, ar[i]));
+                }
+            }
+
+            return new string(ar);
+        }
+    }
+}
