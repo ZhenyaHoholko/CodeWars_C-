657f2dc8df40b4befd680861e1062e5c0d553895
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-27 21:10:53 +0300

    Add files via upload

---
+++ 7kyu_AlternateCapitalization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string[] Capitalize(string s)
+    {
+        string evenIndexed = "";
+        string oddIndexed = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                evenIndexed += char.ToUpper(s[i]);
+                oddIndexed += s[i];
+            }
+            else
+            {
+                evenIndexed += s[i];
+                oddIndexed += char.ToUpper(s[i]);
+            }
+        }
+
+        return new string[] { evenIndexed, oddIndexed };
+    }
+}
