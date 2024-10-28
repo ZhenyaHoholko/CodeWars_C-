6f1ba3640ee7321ac23c2d73b6215813992b840b
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-28 17:40:06 +0300

    Add files via upload

---
+++ 8kyu_NeedleInHaystack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Kata
+{
+    public static string FindNeedle(object[] haystack)
+    {
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] is string && haystack[i].ToString() == "needle")
+            {
+                return $"found the needle at position {i}";
+            }
+        }
+        return "needle not found";
+    }
+}
