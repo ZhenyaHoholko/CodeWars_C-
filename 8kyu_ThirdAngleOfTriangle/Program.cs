900e34c78f82a66d9396388aee48433b732ad9f5
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-10-29 21:06:53 +0300

    Add files via upload

---
+++ 8kyu_ThirdAngleOfTriangle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int OtherAngle(int a, int b)
+    {
+        if((a+b)>0 && (a + b) < 180)
+        {
+            return 180-(a + b);
+        }
+        else { throw new NotImplementedException(); }
+        
+    }
+}
