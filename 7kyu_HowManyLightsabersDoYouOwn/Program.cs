ebe63a6df839ae764f4ecd9f9e83e9474a350c3a
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-11-04 18:02:12 +0300

    Add files via upload

---
+++ 7kyu_HowManyLightsabersDoYouOwn/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string HighAndLow(string numbers)
+    {
+        int[] nums = numbers.Split(' ').Select(int.Parse).ToArray();
+        int max = nums.Max();
+        int min = nums.Min();
+        return $"{max} {min}";
+    }
+}
