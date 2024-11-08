2b26229af9efd605af4eed42a26c284603a58757
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-11-08 20:33:30 +0300

    Add files via upload

---
+++ 8kyu_SumWithoutHighestLowestNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int Sum(int[] numbers)
+    {
+        if (numbers == null || numbers.Length <= 1)
+        {
+            return 0;
+        }
+        int sum = numbers.Sum();
+        int min = numbers.Min();
+        int max = numbers.Max();
+        return sum - min - max;
+    }
+}
