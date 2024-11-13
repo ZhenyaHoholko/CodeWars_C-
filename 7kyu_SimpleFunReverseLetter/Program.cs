04626c095f75aa5cc820faf1270ee79f1e8f1686
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-11-13 16:18:01 +0300

    Add files via upload

---
+++ 7kyu_SimpleFunReverseLetter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+namespace myjinxin
+{
+    public class Kata
+    {
+        public string ReverseLetter(string str)
+        {
+            return new string(str.Where(char.IsLetter).Reverse().ToArray());
+        }
+    }
+}
