82a7988ea6df534f26bd74da6c822b236c4547b5
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2024-12-06 23:15:21 +0300

    Add files via upload

---
+++ 6kyu_DuckTyping/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharpEssentials
+{
+    public class Flat
+    {
+        public int Number { get; set; }
+        public int RoomsCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"Квартира с номером {Number} имеет {RoomsCount} комнат";
+        }
+    }
+    public class Entrance : IEnumerable, IEnumerator
+    {
+        private readonly Flat[] flats;
+        private int index = -1;
+        public Entrance(Flat[] flats)
+        {
+            this.flats = flats;
+        }
+        public object Current
+        {
+            get
+            {
+                if (index < 0 || index >= flats.Length)
+                    throw new InvalidOperationException();
+                return flats[index];
+            }
+        }
+        public bool MoveNext()
+        {
+            index++;
+            return index < flats.Length;
+        }
+        public void Reset()
+        {
+            index = -1;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            Reset();
+            return this;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var flats = new Flat[]
+            {
+                new Flat { Number = 1, RoomsCount = 2 },
+                new Flat { Number = 2, RoomsCount = 3 },
+                new Flat { Number = 3, RoomsCount = 4 }
+            };
+
+            var entrance = new Entrance(flats);
+
+            foreach (Flat flat in entrance)
+            {
+                Console.WriteLine(flat);
+            }
+        }
+    }
+}
