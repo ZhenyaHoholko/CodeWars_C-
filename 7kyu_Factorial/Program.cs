d3595a6fdae30c5d1a603363e399c4cb65e5b070
ZhenyaHoholko <122923970+ZhenyaHoholko@users.noreply.github.com>
2025-01-10 10:22:53 +0300

    Add files via upload

---
+++ 7kyu_Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int Factorial(int n)
+    {
+        if (n < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n));
+        }
+        if (n <= 0 || n == 1)
+            return 1;
+        else
+            return n * Factorial(n - 1);
+    }
+}
